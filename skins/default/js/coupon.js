3ba9de1b6188ef074ebb887d158375d3b60548a3
Wincomi <admin@wincomi.com>
2017-12-10 16:27:28 +0900

    새로운 기본 스킨

--- skins/default/js/coupon.js
+++ skins/default/js/coupon.js
@@ -1,8 +1,8 @@
 function completeUseCoupon(ret_obj) {
-    var error = ret_obj['error'];
-    var message = ret_obj['message'];
-
-    alert(message);
-
-    window.close();
+	var error = ret_obj['error'];
+	var message = ret_obj['message'];
+	
+	alert(message);
+	
+	window.close();
 }
